53bf7985b85efbc762f1e86276309388f2b3c257
thopkins96 <thopkinsmarketingandseo@gmail.com>
2025-09-04 14:16:34 -0500

    Working on CLI

--- src/name_photo_matching/main.py
+++ src/name_photo_matching/main.py
@@ -1,17 +1,42 @@
 import traceback
-import argparse
+import click
 
 from .clip_functions import CLIPHandler
 from .utils import to_csv
 from .gcs import CloudStorageHandler
 from loguru import logger
+from typing import Optional
 
+
+def init(bucket: str, service_account_path: Optional[str] = None):
+    logger.info("Creating GCS client instance.")
+    if service_account_path:
+        logger.info("Using service account to create credentials")
+        try:
+            client = CloudStorageHandler(bucket, service_account_path)
+
+            return client
+
+        except Exception as e:
+            traceback.print_exc()
+            logger.error(f"Creating credentials with service account failed: {e}")
+
+    else:
+        from google.cloud import storage
+        logger.info("Service account path was not passed. Using Google ADC instead.")
+
+        try:
+            client = storage.Client()
+            return client
+
+        except Exception as e:
+            traceback.print_exc()
+            logger.error(f"Creating credentials with Google ADC failed {e}")
 
 def main():
     try:
         logger.info("Initializing main workflow...")
-        client = CloudStorageHandler("cults_files",
-                                     "cloud_storage_service_account.json")
+        client = init_gcs_client()
 
         file_name_map = client.get_3mfs_and_parse_file_names()
         to_csv(file_name_map, "current_cults_files.csv")
@@ -23,4 +48,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    app()
